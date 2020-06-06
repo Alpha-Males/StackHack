ab3b9b8f35fda26ae1cafe99a0ff097be8456cea
princekrroshan01 <princekrroshan01@gmail.com>
2020-06-06 18:55:48 +0530

    added email verification and sending email every morning about the due task

--- todo/routes.py
+++ todo/routes.py
@@ -14,18 +14,20 @@ logout -->
 
 """
 
-# User Auth with REST architecture is provided only after doing some research.
-# Apart from User Auth everything must interact with ORM throug a middleware having REST architecture
 
 
-# key = crpt.key_derive(b"data")  # api to calculate key using PKCS7
-# crpt.key_verify(b"data", key)  # api to verify the key
+
 
 from datetime import datetime
 import os
 import requests
 import secrets
 import hashlib
+import smtplib, ssl
+import re
+import socket
+import smtplib
+import dns.resolver
 
 # import urllib
 
@@ -51,6 +53,56 @@ app.register_blueprint(github_bp, url_prefix="/login")
 
 class RouteException(Exception):
     pass
+
+
+def verify_email(email):
+    """
+
+    """
+    records = dns.resolver.query('emailhippo.com', 'MX')
+    mxRecord = records[0].exchange
+    mxRecord = str(mxRecord)
+
+    # Get local server hostname
+    host = socket.gethostname()
+
+    # SMTP lib setup (use debug level for full output)
+    server = smtplib.SMTP()
+    server.set_debuglevel(0)
+
+    # SMTP Conversation
+    server.connect(mxRecord)
+    server.helo(host)
+    server.mail('smtp@gmail.com')
+    code, message = server.rcpt(email)
+    server.quit()
+
+    return code
+
+def send_email(curr_user):
+    user=User.query.filter_by(id=curr_user).first()
+    task=Tasks.query.filter_by(user_id=curr_user).all()
+    port = 465  
+    smtp_server = "smtp.gmail.com"
+    sender_email = "stackhackforme@gmail.com"  
+    receiver_email = user.email  
+    password = 'stackhack@123$'
+    
+    task_info=[]
+    for i in task:
+        task_info.append(i.title)
+    message = """\
+    Subject: Task for the day.
+
+    hi {} hopes you are doing fine
+    here are your task for the day.
+    Tasks {}.""".format(user.username,task_info)
+
+    context = ssl.create_default_context()
+    
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
 
 
 
@@ -92,7 +144,13 @@ def save_and_upload(file):
 @app.route("/account", methods=["GET", "POST"])
 @login_required
 def account():
-    pro_pic = url_for("static", filename="profile_pics/" + current_user.image_file)
+    curr_user = current_user.id
+    pro_pic = url_for("static", filename="profile_pics/" + current_user.image_file)    
+    var = request.args.get("my_var")
+    
+    if var == "send_mail":
+        send_email(curr_user)
+    
     if request.method == "POST":
         file = request.files.get("file")
         if file:
@@ -150,24 +208,26 @@ def login():
 def register():
     if current_user.is_authenticated:
         return redirect(url_for("home"))
-    fields = {}
-    fields["route"] = "register"
-
+    #fields = {}
+    #fields["route"] = "register"
+	    
     if request.method == "POST":
         username = request.form.get("username")
         email = request.form.get("email")
-        password = request.form.get("password")
-        confirm = request.form.get("confirm")
-
-        if password == confirm and checkavl(email, username):
-            hashed_password = hashlib.sha224(password.encode("utf-8")).hexdigest()
-            user = User(username=username, email=email, password=hashed_password)
-            db.session.add(user)
-            db.session.commit()
-            return redirect(url_for("login"))
-
+        match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email)
+        if match == None or verify_email(email)!=250:
+            flash(u"please provide a valid email address", "error")
         else:
-            flash(u"validation error", "error")
+            password = request.form.get("password")
+            confirm = request.form.get("confirm")
+            if password == confirm and checkavl(email, username):
+                hashed_password = hashlib.sha224(password.encode("utf-8")).hexdigest()
+                user = User(username=username, email=email, password=hashed_password)
+                db.session.add(user)
+                db.session.commit()
+                return redirect(url_for("login"))
+            else:
+                flash(u"validation error", "error")
 
     return render_template("register.html")
 
